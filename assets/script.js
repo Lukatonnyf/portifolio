2dd978759a6ab17a1100279e5b2f430b239debb9
Lukatonnyf <124458583+Lukatonnyf@users.noreply.github.com>
2024-03-05 00:42:55 -0300

    portifoliopessoal

---
+++ assets/script.js
@@ -0,0 +1,60 @@
+// const anoAtual = new Date();
+// const meuAniversario = new Date("2007-09-16");
+// const birth_date = new Date("2007-09-16").toISOString();
+
+
+// const btn = document.querySelector('.toggle');
+// const body = document.body;
+
+// btn.addEventListener('click', () => {
+//     document.body.style.background = "#a9a9a9"
+// })
+
+
+function lightmode() {
+    if (document.body.style.background === ``) {
+        document.body.style.background = 'green'
+    } else {
+        document.body.style.background = "#12112"
+    }
+}
+
+
+const myObserver = new IntersectionObserver((animationhabilits) => {
+    animationhabilits.forEach((entry) => {
+        if (entry.isIntersecting === true) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const habilitAnimation = document.querySelectorAll('.habilitcards')
+
+habilitAnimation.forEach((element) => myObserver.observe(element))
+
+
+
+const showTohide = new IntersectionObserver((projectanimations) => {
+    projectanimations.forEach((yousee) => {
+        if (yousee.isIntersecting === true) {
+            yousee.target.classList.add('visible')
+        } else {
+            yousee.target.classList.remove('visible')
+        }
+
+    })
+})
+
+const projectscards = document.querySelectorAll('.everyprojects')
+
+projectscards.forEach((element) => showTohide.observe(element))
+
+
+const btnIrparaoSite = document.getElementById('btnsite')
+
+
+function irprojecthonor(btnIrparaoSite) {
+    window.location.href = ""
+}
